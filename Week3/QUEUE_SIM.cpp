330a5d465cc01bc7abf9affac30c9e5b743dbc04
thanh1110017d4 <thanhsktbox@gmail.com>
2024-12-17 21:22:51 +0700

    50%

---
+++ Week3/QUEUE_SIM.cpp
@@ -0,0 +1,30 @@
+//C++
+#include <bits/stdc++.h>
+
+using namespace std;
+
+queue<int> q;
+string v;
+int a;
+
+int main()
+{
+    do{
+        cin >> v;
+        if (v == "PUSH")
+        {
+            cin >> a;
+            q.push(a);
+        }
+        else if (v == "POP")
+        {
+            if (!q.empty())
+            {
+                cout << q.front() << endl;
+                q.pop();
+            }
+            else cout << "NULL" << endl;
+        }
+    } while (v != "#");
+    return 0;
+}
